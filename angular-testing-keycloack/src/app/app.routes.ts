ad220e0de866ef502737e3d2bc1df8d8e6a6b1fa
DESKTOP-8QHVFT5\Badis Ben Sassi <badisbensassi8@gmail.com>
2024-09-04 12:40:30 +0100

    deleted test based projects

--- angular-testing-keycloack/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AgencyManagementComponent } from "./agency-management/agency-management.component"
-import { LoginComponent } from "./login/login.component";
-import { AuthGuard } from './auth.guard';
-export const routes: Routes = [
-    { path: 'protected', component: AgencyManagementComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: '**', redirectTo: 'login' }
-  ];
